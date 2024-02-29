f71185caaeb6879002ad950f0ae490eca65155bc
nvsick <nvs.holy@gmail.com>
2024-02-29 22:34:19 +0300

    change url validation in LinkFactoryImpl

--- bot/src/main/java/edu/java/bot/entity/factory/impl/LinkFactoryImpl.java
+++ bot/src/main/java/edu/java/bot/entity/factory/impl/LinkFactoryImpl.java
@@ -5,9 +5,10 @@ import edu.java.bot.entity.enums.LinkDomain;
 import edu.java.bot.entity.factory.LinkFactory;
 import java.io.IOException;
 import java.net.ConnectException;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import org.apache.commons.validator.routines.UrlValidator;
+import java.net.URL;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,17 +20,19 @@ public class LinkFactoryImpl implements LinkFactory {
     @Override
     @Transactional
     public Link createLink(String url) throws URISyntaxException, ConnectException {
-        if (!isValidLink(url)) {
+        String fullUrl = ((!url.startsWith(HTTP) && !url.startsWith(HTTPS)) ? HTTPS : "") + url;
+
+        if (!isValidLink(fullUrl)) {
             throw new URISyntaxException(url, "Given url is not correct");
         }
 
-        if (!isReachableUrl(url)) {
+        if (!isReachableUrl(fullUrl)) {
             throw new ConnectException("Website is not reachable");
         }
 
         for (LinkDomain linkDomain : LinkDomain.values()) {
-            if (linkDomain.isDomainForUrl(url)) {
-                return new Link(linkDomain, url);
+            if (linkDomain.isDomainForUrl(fullUrl)) {
+                return new Link(linkDomain, fullUrl);
             }
         }
 
@@ -37,17 +40,18 @@ public class LinkFactoryImpl implements LinkFactory {
     }
 
     private boolean isValidLink(String url) {
-        String fullUrl = ((!url.startsWith(HTTP) && !url.startsWith(HTTPS)) ? HTTPS : "") + url;
-
-        UrlValidator validator = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
-        return validator.isValid(fullUrl);
+        try {
+            URI uri = new URI(url);
+            URL validatedURL = uri.toURL();
+            return true;
+        } catch (MalformedURLException | URISyntaxException e) {
+            return false;
+        }
     }
 
     private boolean isReachableUrl(String url) {
-        String fullUrl = ((!url.startsWith(HTTP) && !url.startsWith(HTTPS)) ? HTTPS : "") + url;
-
         try {
-            new URI(fullUrl).toURL().openStream().close();
+            new URI(url).toURL().openStream().close();
             return true;
         } catch (IOException | URISyntaxException e) {
             return false;
